679661c07b219f4871f007ea75e89b27019eabe3
Derp2349 <restrepo.danielito@gmail.com>
2022-01-13 09:49:50 -0500

    Modified Gamepanel.java
    modified GamePanel.java to load player stats into player classes, temporarily removed the code inside of loadData()

--- GamePanel.java
+++ GamePanel.java
@@ -11,7 +11,8 @@ public class GamePanel extends JPanel implements ActionListener{
 	ArrayList<Double> times = new ArrayList<>();
 	Color[] col = new Color[2];
 	Color backCol = Color.white;
-	int[][] stats = new int[2][4];
+	Player phost = new Player("Keanu Reeves",25,150,75,5,100,50,0);
+	Player pclient = new Player("Eggs",10,100,50,10,100,50,0);
 	int p1HP = 29;
 	int p2HP = 15;
 	int p1En = 15;
@@ -19,44 +20,32 @@ public class GamePanel extends JPanel implements ActionListener{
 	int maxEn = 100;
 	
 	public void paintComponent(Graphics g) {
-		// hardcode testing, del later
-		stats[0][1] = 30;
-		stats[1][1] = 40;
-		
-		double HPer1 = p1HP*1.0/stats[0][1];
-		double HPer2 = p2HP*1.0/stats[1][1];
-		double EPer1 = p1En*1.0/maxEn;
-		double EPer2 = p2En*1.0/maxEn;
-		
+				
 		// background
 		g.setColor(backCol);
 		g.fillRect(0, 0, 1280, 720);
 		
 		// hp and energy
 		g.setColor(col[0]);
-		g.fillRect(0, 0,(int) (400*HPer1), 50);
-		g.fillRect(1280-(int)(400*HPer2), 0, 400, 50);
+		g.fillRect(0, 0,(int)(400*phost.intchealth/phost.intphealth), 50);
+		g.fillRect(1280-(int)(400*pclient.intchealth/pclient.intphealth), 0, 400, 50);
 		
 		g.setColor(col[1]);
-		g.fillRect(0, 50, (int)(300*EPer1), 25);
-		g.fillRect(1280-(int)(300*EPer2), 50, 300, 25);
+		g.fillRect(0, 50, (int)(5*phost.intcenergy), 25);
+		g.fillRect(1280-(int)(5*pclient.intcenergy), 50, 300, 25);
 		
 		
 	}
 	
-	public void loadData(String[][] data, int p1, int p2) {
-		for (int intC=0;intC<4;intC++) {
-			stats[0][intC] = Integer.parseInt(data[p1][intC]);
-		}
-		for (int intC=0;intC<4;intC++) {
-			stats[1][intC] = Integer.parseInt(data[p2][intC]);
-		}
+	public void loadData(String[][] data) {
+		
 	}
 	
 	public GamePanel() {
 		super();
 		col[0] = Color.red;
 		col[1] = Color.blue;
+		time.start();
 	}
 
 	@Override
