146436fbb600f624d233f4e983dcf6a30c9a9bd9
Derp2349 <restrepo.danielito@gmail.com>
2022-01-16 15:11:51 -0500

    reworked the menu (WIP)
    menu now contains less background layers so it should be easier to design

--- MenuPanel.java
+++ MenuPanel.java
@@ -41,7 +41,6 @@ public class MenuPanel extends JPanel implements ActionListener{
 	int hovered = -1;
 	
 	int lastClick = -1;
-	int ticksSince = 0;
 	int atkTicks = 0;
 	int atkCd = 0;
 	
@@ -58,132 +57,115 @@ public class MenuPanel extends JPanel implements ActionListener{
 	}
 	
 	public void paintComponent(Graphics g){
-		pXDtarg = (int)((mousePos.x-640)*1-cameraPos.x);
-		pYDtarg = (int)((mousePos.y-360)*1-cameraPos.y);
+		pXDtarg = (int)((mousePos.x-640)*0.1-cameraPos.x);
+		pYDtarg = (int)((mousePos.y-360)*0.1-cameraPos.y);
 		menudraw.setColor(Color.BLACK);
 		pXDist = (int)(0.8*pXDist+0.2*pXDtarg);
 		pYDist = (int)(0.8*pYDist+0.2*pYDtarg);
 		menudraw.fillRect(0,0,1280,720);
 		
-		//background
+		//backgrounds
 		menudraw.setColor(new Color(237,237,237));
-		menudraw.fillRect(-320-(int)(pXDist*0.5),-4600-(int)(pYDist*0.5),1920,5600);
-		menudraw.fillRect(-7360-(int)(pXDist*0.5),-180-(int)(pYDist*0.5),16000,1080);
-		menudraw.setColor(new Color(255,255,255));
-		menudraw.fillOval(320-(int)(pXDist*0.5),50-(int)(pYDist*0.5),640,640);
-		
-		//ground layers
+		menudraw.fillRect(0-pXDist,0-pYDist,1280,720);
 		menudraw.setColor(new Color(202,202,202));
-		menudraw.fillRect(-256-(int)(pXDist*0.4),446-(int)(pYDist*0.4),1792,300);
+		menudraw.fillRect(0-pXDist,-792-pYDist,1280,720);
 		menudraw.setColor(new Color(185,185,185));
-		menudraw.fillRect(-192-(int)(pXDist*0.3),533-(int)(pYDist*0.3),1664,300);
+		menudraw.fillRect(1408-pXDist,0-pYDist,1280,720);
 		menudraw.setColor(new Color(170,170,170));
-		menudraw.fillRect(-128-(int)(pXDist*0.2),576-(int)(pYDist*0.2),1536,300);
+		menudraw.fillRect(0-pXDist,-792-pYDist,1280,720);
 		menudraw.setColor(new Color(140,140,140));
-		menudraw.fillRect(-96-(int)(pXDist*0.15),605-(int)(pYDist*0.15),1472,1357);
-		
-		//clouds
-		menudraw.setColor(new Color(245,245,245));
-		menudraw.fillRect(-192-(int)(pXDist*0.3),-2000-(int)(pYDist*0.3),1664,1900);
-		menudraw.fillRect(-192-(int)(pXDist*0.3),-2600-(int)(pYDist*0.3),1664,300);
-		menudraw.setColor(new Color(250,250,250));
-		menudraw.fillRect(-128-(int)(pXDist*0.2),-1000-(int)(pYDist*0.2),1536,900);
-		menudraw.setColor(new Color(255,255,255));
-		menudraw.fillRect(-96-(int)(pXDist*0.15),-500-(int)(pYDist*0.15),1472,400);
+		menudraw.fillRect(-1408-pXDist,0-pYDist,1280,720);
 		
 		//change menu buttons position
-		buttons[0].setLocation(144-(int)(pXDist*0.1),420-(int)(pYDist*0.1));
-		buttons[1].setLocation(656-(int)(pXDist*0.1),420-(int)(pYDist*0.1));
-		buttons[2].setLocation(826-(int)(pXDist*0.1),576-(int)(pYDist*0.1));
-		buttons[3].setLocation(485-(int)(pXDist*0.1),576-(int)(pYDist*0.1));
-		buttons[4].setLocation(144-(int)(pXDist*0.1),576-(int)(pYDist*0.1));
+		buttons[0].setLocation(144-(int)(pXDist*1.5),420-(int)(pYDist*1.5));
+		buttons[1].setLocation(656-(int)(pXDist*1.5),420-(int)(pYDist*1.5));
+		buttons[2].setLocation(826-(int)(pXDist*1.5),576-(int)(pYDist*1.5));
+		buttons[3].setLocation(485-(int)(pXDist*1.5),576-(int)(pYDist*1.5));
+		buttons[4].setLocation(144-(int)(pXDist*1.5),576-(int)(pYDist*1.5));
 			
-		buttons[5].setLocation(30-(int)(pXDist*0.1),-172-(int)(pYDist*0.1));
-		buttons[6].setLocation(555-(int)(pXDist*0.1),812-(int)(pYDist*0.1));
-		buttons[7].setLocation(1438-(int)(pXDist*0.1),600-(int)(pYDist*0.1));
-		buttons[8].setLocation(-328-(int)(pXDist*0.1),600-(int)(pYDist*0.1));
+		buttons[5].setLocation(30-(int)(pXDist*1.5),-622-(int)(pYDist*1.5));
+		buttons[6].setLocation(555-(int)(pXDist*1.5),1262-(int)(pYDist*1.5));
+		buttons[7].setLocation(2238-(int)(pXDist*1.5),600-(int)(pYDist*1.5));
+		buttons[8].setLocation(-1128-(int)(pXDist*1.5),600-(int)(pYDist*1.5));
 		
 		r.x += defX;
 		r.y+=defY;
 		
-		// wait for transition
-		if (ticksSince>7) {
-			// bottom middle button on main menu
-			if (lastClick==3) {
-				// dummy hitbox
-				menudraw.setColor(Color.red);
-				menudraw.fillRect(dummy.x, dummy.y, dummy.width, dummy.height);
-				// training fighter hitbox
-				menudraw.setColor(Color.black);
-				menudraw.fillRect(r.x, r.y, r.width, r.height);
-				menudraw.drawString("Q - High attack", 200, 50);
-				menudraw.drawString("E - Low attack", 700, 50);
-				if (atking) {
-					if (atkTicks<10) {
-						if (left) {
-							menudraw.setColor(Color.blue);
-							menudraw.fillRect(atks[up].x, atks[up].y, atks[up].width, atks[up].height);
-						} else {
-							menudraw.setColor(Color.blue);
-							menudraw.fillRect(atks[up].x, atks[up].y, atks[up].width, atks[up].height);
-						}
-					} else if (atkTicks==10) {
-						atkTicks=-1;
-						atking = false;
-						atkCd=40;
+		// bottom middle button on main menu
+		if (lastClick==3) {
+			// dummy hitbox
+			menudraw.setColor(Color.red);
+			menudraw.fillRect(dummy.x+1472-pXDist, dummy.y-pYDist, dummy.width, dummy.height);
+			// training fighter hitbox
+			menudraw.setColor(Color.black);
+			menudraw.fillRect(r.x+1472-pXDist, r.y-pYDist, r.width, r.height);
+			menudraw.drawString("Q - High attack", 1772-pXDist, 50-pYDist);
+			menudraw.drawString("E - Low attack", 2200-pXDist, 50-pYDist);
+			if (atking) {
+				if (atkTicks<10) {
+					if (left) {
+						menudraw.setColor(Color.blue);
+						menudraw.fillRect(atks[up].x+1472-pXDist, atks[up].y-pYDist, atks[up].width, atks[up].height);
+					} else {
+						menudraw.setColor(Color.blue);
+						menudraw.fillRect(atks[up].x+1472-pXDist, atks[up].y-pYDist, atks[up].width, atks[up].height);
 					}
-					atkTicks++;
-				} else if (atkCd>0) {
-					atkCd--;
+				} else if (atkTicks==10) {
+					atkTicks=-1;
+					atking = false;
+					atkCd=40;
 				}
-			} 
-			// bottom right on main menu
-			else if (lastClick==2) {
-				menudraw.drawImage(cred, 0, 0, null);
-			} 
-			
-			// bottom left on main menu
-			else if (lastClick==4) {
-				menudraw.setColor(Color.black);
-				menudraw.drawString("High scores (fastest round win in (s))", 400, 50);
-				menudraw.drawString("Username", 50, 100);
-				menudraw.drawString("Time (s)", 1280-250, 100);
+				atkTicks++;
+			} else if (atkCd>0) {
+				atkCd--;
+			}
+		} 
+		// bottom right on main menu
+		else if (lastClick==2) {
+			menudraw.drawImage(cred, 0, 0, null);
+		} 
+		
+		// bottom left on main menu
+		else if (lastClick==4) {
+			menudraw.setColor(Color.black);
+			menudraw.drawString("High scores (fastest round win in (s))", 400, 50);
+			menudraw.drawString("Username", 50, 100);
+			menudraw.drawString("Time (s)", 1280-250, 100);
+		}
+		
+		// host game button & join game button
+		else if (lastClick==0 || lastClick == 1) {
+		 // draw character select buttons
+			for (int i=0;i<4;i++) select[i].setLocation(100+(200*(i%2)), 100+(200*(i/2)));
+			for (int i=0;i<4;i++) {
+				if (select[i].changelook(mousePos, mouseDown, retriggerCatch)){
+					cameraPos = select[i].pwp;
+					retriggerCatch = true;
+					
+					// find which button was clicked
+					selected = selected == i ? -1 : i;
+					hovered = -1;
+				} else if (!mouseDown) {
+					retriggerCatch = false;
+				} 
+				
+				// check if button is being hovered over
+				if (select[i].contains(mousePos)&&!select[i].changelook(mousePos, mouseDown, retriggerCatch)) {
+					hovered = i;
+				} else if (hovered!=-1) {
+					if (!select[hovered].contains(mousePos)) hovered = -1;
+				}
+				menudraw.drawImage(select[i].drawme,(int)select[i].getX(),(int)select[i].getY(),null);
 			}
 			
-			// host game button & join game button
-			else if (lastClick==0 || lastClick == 1) {
-			 // draw character select buttons
-				for (int i=0;i<4;i++) select[i].setLocation(100+(200*(i%2)), 100+(200*(i/2)));
-				for (int i=0;i<4;i++) {
-					if (select[i].changelook(mousePos, mouseDown, retriggerCatch)){
-						cameraPos = select[i].pwp;
-						retriggerCatch = true;
-						
-						// find which button was clicked
-						selected = selected == i ? -1 : i;
-						hovered = -1;
-					} else if (!mouseDown) {
-						retriggerCatch = false;
-					} 
-					
-					// check if button is being hovered over
-					if (select[i].contains(mousePos)&&!select[i].changelook(mousePos, mouseDown, retriggerCatch)) {
-						hovered = i;
-					} else if (hovered!=-1) {
-						if (!select[hovered].contains(mousePos)) hovered = -1;
-					}
-					menudraw.drawImage(select[i].drawme,(int)select[i].getX(),(int)select[i].getY(),null);
-				}
-				
-				// if a selection/hover has been made, draw the appropriate image
-				if (selected!=-1) {
-					menudraw.drawImage(selectImg[2], 100+(200*(selected%2)), 100+(200*(selected/2)), null);
-				} 
-				if (hovered!=-1) {
-					menudraw.drawImage(selectImg[0], 100+(200*(hovered%2)), 100+(200*(hovered/2)), null);
-				}
+			// if a selection/hover has been made, draw the appropriate image
+			if (selected!=-1) {
+				menudraw.drawImage(selectImg[2], 100+(200*(selected%2)), 100+(200*(selected/2)), null);
 			} 
-		}
+			if (hovered!=-1) {
+				menudraw.drawImage(selectImg[0], 100+(200*(hovered%2)), 100+(200*(hovered/2)), null);
+			}
+		} 
 		
 		//draw menu buttons
 		for (int i = 0; i<buttons.length; i++){
@@ -191,15 +173,12 @@ public class MenuPanel extends JPanel implements ActionListener{
 				cameraPos = buttons[i].pwp;
 				retriggerCatch = true;
 				lastClick = i;
-				ticksSince = 0;
 			} else if (!mouseDown) {
 				retriggerCatch = false;
 			}
 			menudraw.drawImage(buttons[i].drawme,(int)buttons[i].getX(),(int)buttons[i].getY(),null);
 		}
 		g.drawImage(menuscreen,0,0,null);
-		ticksSince++;
-		if (ticksSince==Integer.MAX_VALUE)ticksSince=80;
 	}
 	
 	public BufferedImage img(String fileName) {
@@ -221,11 +200,11 @@ public class MenuPanel extends JPanel implements ActionListener{
 		menudraw.setFont(new Font("Small Fonts",Font.PLAIN,30));
 		
 		//switch to menu buttons
-		buttons[0] = new phButt(0,0,480,130,0,7920,"b1def.png","b1hov.png","b1prs.png");
-		buttons[1] = new phButt(0,0,480,130,0,7920,"b1def.png","b1hov.png","b1prs.png");
-		buttons[2] = new phButt(0,0,310,86,0,-7920,"b1def.png","b1hov.png","b1prs.png");
-		buttons[3] = new phButt(0,0,310,86,-14080,0,"b1def.png","b1hov.png","b1prs.png");
-		buttons[4] = new phButt(0,0,310,86,14080,0,"b1def.png","b1hov.png","b1prs.png");
+		buttons[0] = new phButt(0,0,480,130,0,828,"b1def.png","b1hov.png","b1prs.png");
+		buttons[1] = new phButt(0,0,480,130,0,828,"b1def.png","b1hov.png","b1prs.png");
+		buttons[2] = new phButt(0,0,310,86,0,-828,"b1def.png","b1hov.png","b1prs.png");
+		buttons[3] = new phButt(0,0,310,86,-1472,0,"b1def.png","b1hov.png","b1prs.png");
+		buttons[4] = new phButt(0,0,310,86,1472,0,"b1def.png","b1hov.png","b1prs.png");
 		
 		//return to menu buttons
 		buttons[5] = new phButt(0,0,170,90,0,0,"b1def.png","b1hov.png","b1prs.png");
@@ -235,7 +214,7 @@ public class MenuPanel extends JPanel implements ActionListener{
 		
 		// character select buttons
 		for (int i=0;i<4;i++) {
-			select[i] = new phButt(0, 0, 200, 200,0,7920,"f"+i+"def.jpg","f"+i+"def.jpg","f"+i+"def.jpg");
+			select[i] = new phButt(0, 0, 200, 200,0,828,"f"+i+"def.jpg","f"+i+"def.jpg","f"+i+"def.jpg");
 		}
 		
 		// high atk
